fbdd3b193f481481d179e83dc5c7d7596245fdb3
JohnCB <anhbid1000@gmail.com>
2026-04-22 12:30:24 +0700

    test environment variable

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 export default function Home() {
-  const [socketId, setSocketId] = useState('');
+  const [socketId, setSocketId] = useState("");
 
   useEffect(() => {
     // Kết nối tới cổng 5000 của Backend
-    const socket = io('http://localhost:5000');
+    const socket = io("http://localhost:5000");
 
-    socket.on('connect', () => {
-      console.log('Đã kết nối Socket thành công!');
-      setSocketId(socket.id ?? '');
+    socket.on("connect", () => {
+      console.log("Đã kết nối Socket thành công!");
+      setSocketId(socket.id ?? "");
     });
 
     // Dọn dẹp (cleanup) khi tắt component
@@ -24,8 +24,11 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="text-3xl font-bold mb-4">MERN Video Conference Test</h1>
+      <div>
+        <h1>Đang kết nối tới: {process.env.NEXT_PUBLIC_BACKEND_URL}</h1>
+      </div>
       <p className="text-lg">
-        Trạng thái Socket.io:{' '}
+        Trạng thái Socket.io:{" "}
         {socketId ? (
           <span className="text-green-600 font-bold">
             🟢 Đã kết nối (ID: {socketId})
